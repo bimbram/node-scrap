dad272a25eec6911bcc88782fb47b88b89aa4eb4
Bimo Bramantyo <bim.bramantyo@gmail.com>
2019-01-26 03:18:23 +0700

    finished lesson 3, writing a simple imdb scraper

---
+++ simple-imdb-scraper/index.js
@@ -0,0 +1,17 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const URL = 'https://www.imdb.com/title/tt0468569/?ref_=nv_sr_1';
+
+(async () => {
+  const response = await request(URL);
+  // console.log(response);
+
+  let $ = cheerio.load(response);
+  
+  let title = $('div[class="title_wrapper"] > h1').text();
+  let rating = $('span[itemprop="ratingValue"]').text();
+  console.log(title);
+  console.log(rating);
+
+})();
