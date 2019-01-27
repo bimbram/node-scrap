bae9c3dcca50dfc2c858fc6b1af23b7bd19e7402
Bimo Bramantyo <bim.bramantyo@gmail.com>
2019-01-27 10:29:11 +0700

    finished lesson 12, multiple movies and saving JSON to file

--- simple-imdb-scraper/index.js
+++ simple-imdb-scraper/index.js
@@ -1,42 +1,64 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
+const fs = require('fs');
 
-const URL = "https://www.imdb.com/title/tt0468569/?ref_=nv_sr_1";
+
+const URLS = [
+  "https://www.imdb.com/title/tt0468569/?ref_=nv_sr_1",
+  "https://www.imdb.com/title/tt0796366/"
+];
 
 (async () => {
-  const response = await request({
-    url: URL,
-    headers: {
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Accept-Language": "en-US,en;q=0.9",
-      "Cache-Control": "no-cache",
-      Connection: "keep-alive",
-      Host: "www.imdb.com",
-      Pragma: "no-cache",
-      "Upgrade-Insecure-Requests": "1",
-      "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.64 Safari/537.36"
-    },
-    gzip: true
-  });
-  let $ = cheerio.load(response);
+  let moviesData = [];
+  for (let movie of URLS) {
+    const response = await request({
+      uri: movie,
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+        Host: "www.imdb.com",
+        Pragma: "no-cache",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.64 Safari/537.36"
+      },
+      gzip: true
+    });
+    let $ = cheerio.load(response);
 
-  let title = $('div[class="title_wrapper"] > h1').text().trim();
-  let rating = $('span[itemprop="ratingValue"]').text();
-  let poster = $('div[class="poster"] > a > img').attr('src')
-  let totalRatings = $('div[class="imdbRating"] > a').text()
-  let releaseDate = $('a[title="See more release dates"]').text().trim();
-  let genres = [];
-  $('div[class="subtext"] a[href^="/search/title?"]').each((i, elm) => {
-	   let genre = $(elm).text()
-	genres.push(genre);
-})
-  console.log(`title: ${title}, ${rating}, ${poster}, ${totalRatings}, ${releaseDate}`);
-  console.log(`rating: ${rating}`);
-  console.log(`poster: ${poster}`);
-  console.log(`total ratings: ${totalRatings}`);
-  console.log(`release date: ${releaseDate}`);
-  console.log(`genres: ${genres}`);
+    let title = $('div[class="title_wrapper"] > h1')
+      .text()
+      .trim();
+    let rating = $('span[itemprop="ratingValue"]').text();
+    let poster = $('div[class="poster"] > a > img').attr("src");
+    let totalRatings = $('div[class="imdbRating"] > a').text();
+    let releaseDate = $('a[title="See more release dates"]')
+      .text()
+      .trim();
+    let genres = [];
+    $('div[class="subtext"] a[href^="/search/title?"]').each((i, elm) => {
+      let genre = $(elm).text();
+      genres.push(genre);
+    });
+
+    moviesData.push({
+      title,
+      rating,
+      poster,
+      totalRatings,
+      releaseDate,
+      genres
+    });
+  }
+
+  fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
+
+
+  console.log(moviesData);
+
+
 })();
